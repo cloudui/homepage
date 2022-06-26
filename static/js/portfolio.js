fc590323925967abb6bc470fe24f93667cabaf41
eric-chen <eric.chen@mailchimp.com>
2022-06-26 15:51:16 -0400

    Add Animate.css animations for portfolio toggle

--- static/js/portfolio.js
+++ static/js/portfolio.js
@@ -3,20 +3,28 @@
 let toggled = false;
 
 function ptoggle() {
+    let animate = document.getElementById('animate-section');
+    let pfolio = document.getElementsByClassName('portfolio');
+
     if (!toggled) {
         toggled = true;
-        let pfolio = document.getElementsByClassName('portfolio');
-
+        
         for (let i = 0; i < pfolio.length; i++) {
             pfolio[i].style.display = "block"
         }
+        animate.classList.remove('animate__flash')
+        animate.classList.add('animate__bounce');
+
     } else {
-        toggled = false;
-        let pfolio = document.getElementsByClassName('portfolio');
+        toggled = false;        
 
         for (let i = 0; i < pfolio.length; i++) {
             pfolio[i].style.display = "none"
         }
+
+        animate.classList.remove('animate__bounce');
+        animate.classList.add('animate__flash');
+        
     }
 
 };
